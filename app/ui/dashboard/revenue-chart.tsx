e28cf6aec959ac09999bd9c1b6b9017a9be3f0a0
hust-ldg <3305473927@qq.com>
2024-11-11 18:29:36 +0800

    feat:card

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -17,15 +17,16 @@ export default async function RevenueChart({
   const chartHeight = 350;
   // NOTE: Uncomment this code in Chapter 7
 
-  // const { yAxisLabels, topLabel } = generateYAxis(revenue);
+  const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
-  // if (!revenue || revenue.length === 0) {
-  //   return <p className="mt-4 text-gray-400">No data available.</p>;
-  // }
+  if (!revenue || revenue.length === 0) {
+    return <p className="mt-4 text-gray-400">No data available.</p>;
+  }
 
   return (
     <div className="w-full md:col-span-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h2 className={`${lusitana.className} ;''
+      -4 text-xl md:text-2xl`}>
         Recent Revenue
       </h2>
       {/* NOTE: Uncomment this code in Chapter 7 */}
@@ -54,12 +55,33 @@ export default async function RevenueChart({
               </p>
             </div>
           ))}
+        </div> */}
+      <div className="rounded-xl bg-gray-50 p-4">
+        <div className='sm:grid-cols-13 mt-0 grid grid-cols-12 items-end bg-white gap-2 rounded-md p-4 md:gap-4'>
+          <div className='flex-col mb-6 hidden justify-between text-sm text-gray-400 sm:flex '
+          style={{height:`${chartHeight}px`}}
+          >
+            {yAxisLabels.map((label)=>(<p key={label}>{label}</p>))}
+          </div>
+          {revenue.map(month=>(
+            <div key={month.month} className='flex flex-col items-center gap-2'>
+              <div className='w-full bg-blue-300 rounded-md'
+              style={{
+                height:`${(chartHeight/topLabel)*month.revenue}px`
+              }}> 
+              </div>
+              <p className="-rotate-90 text-sm text-gray-400 rotate-0">
+              {month.month}
+              </p>
+            </div>
+          ))}
         </div>
         <div className="flex items-center pb-2 pt-6">
           <CalendarIcon className="h-5 w-5 text-gray-500" />
           <h3 className="ml-2 text-sm text-gray-500 ">Last 12 months</h3>
         </div>
-      </div> */}
-    </div>
+      </div>
+     </div>
+    // </div>
   );
 }
